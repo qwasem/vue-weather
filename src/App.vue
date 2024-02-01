5d233e73016bfaaea99426170fcbf74957d03cd2
qwasem <qasemsianaki@gmail.com>
2024-02-01 10:47:54 +0330

    the third design

--- src/App.vue
+++ src/App.vue
@@ -64,7 +64,16 @@ main{
   outline: none;
   background: none;
 
-  background-color: rgba(255, 255, 255, 0.15);
-  
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+
+.search-box .search-bar:focus{
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 0px 16px 0px;
+
 }
 </style>
