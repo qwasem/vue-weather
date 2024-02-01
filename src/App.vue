72c29178259db30f1ce009f0a498e7984f4b5483
qwasem <qasemsianaki@gmail.com>
2024-02-01 10:40:17 +0330

    the second design

--- src/App.vue
+++ src/App.vue
@@ -33,5 +33,38 @@ body{
 
 #app{
   background-image: url('./assets/cold-bg.jpg');
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s; 
+}
+
+main{
+  min-height: 100vh;
+  padding: 25px;
+
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25) , rgba(0, 0, 0, 0.75));
+}
+
+.search-box{
+  width: 100%;
+  margin-bottom: 30px;
+
+}
+
+.search-box .search-bar{
+  display: block;
+  width: 100%;
+  padding: 15px;
+
+  color: #313131;
+  font-size: 20px;
+
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+
+  background-color: rgba(255, 255, 255, 0.15);
+  
 }
 </style>
