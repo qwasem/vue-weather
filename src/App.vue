bf1abefea8629fec436114af1df671f39b15d191
qwasem <qasemsianaki@gmail.com>
2024-02-01 10:26:31 +0330

    The first design

--- src/App.vue
+++ src/App.vue
@@ -22,5 +22,16 @@ export default {
 </script>
 
 <style>
+* {
+  margin:0 ;
+  padding: 0;
+  box-sizing: border-box;
+}
+body{
+  font-family: 'montserrat' , sans-serif;
+}
 
+#app{
+  background-image: url('./assets/cold-bg.jpg');
+}
 </style>
