a6100fd839396adbffa64bc6cfae096f8a721eb1
qwasem <qasemsianaki@gmail.com>
2024-02-02 10:59:55 +0330

    make the location

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,9 @@
           @keypress="fetchWeather"
         />
       </div>
-      <div class="weather-wrap">
+      <div class="weather-wrap" v-if="(typeof weather.main != 'undefined')">
         <div class="location-box">
-          <div class="location">Northampton, UK</div>
+          <div class="location">{{ weather.name }} , {{ weather.sys.country }}</div>
           <div class="date">Monday 20 January 2023</div>
         </div>
 
@@ -41,7 +41,7 @@ export default {
   methods:{
     fetchWeather(){
       if(e.key = "Enter"){
-        fetch(`${this.api_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
           .then(res =>{
               return res.json();
           }).then(this.setResults);
